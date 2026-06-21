c9969ce8cd0709560856e19177c43ed33df7e8c7
Rett Berg <vitiral@gmail.com>
2026-06-20 19:09:59 -0600

    typo: add score

--- cmd/civgame/typo.lua
+++ cmd/civgame/typo.lua
@@ -6,19 +6,52 @@ local M = mty.mod'civgame.typo'
 local et = require'ele.types'
 local egame = require'ele.game'
 local ds = require'ds'
-local concat = mty.from(table, 'concat')
 local vt100 = require'vt100'
 
-local push = mty.from(table, 'insert')
+local concat  = mty.from(table,   'concat')
+local push    = mty.from(table,   'insert')
+local sfmt    = mty.from(string,  'format')
+local info    = mty.from'ds.log    info'
+local Game, S = mty.from'ele.game  Game,Sprite'
 
-M.Typo = mty.extend(egame.Game, 'Typo', {
+local DONE = 'YOU WIN'
+
+local want = {
+  'do', 'so', 'hi', 'hello', 'at',
+}
+
+M.Typo = mty.extend(Game, 'Typo', {
   'user {chr}: the text the player has input.',
+  'score [int]', score = 0,
+  'wi [int]',    wi = 1,
 })
 
 function M.Typo:draw(ed, isRight)
+  local h = self.th
   ds.clear(self.sprites)
-  push(self.sprites, egame.Sprite{l=1,c=1, txt=concat(self.user)})
-  return egame.Game.draw(self, ed, isRight)
+  push(self.sprites, S{l=h-2,c=1, txt=sfmt('Score: %s', self.score)})
+  push(self.sprites, S{l=h-1,c=1, txt=want[self.wi] or DONE})
+  push(self.sprites, S{l=h  ,c=1, txt=concat(self.user)})
+  return Game.draw(self, ed, isRight)
+end
+
+function M.Typo:keyinput(ed, ev)
+  info('typo keyinput %q', ev)
+  local chr = ev[1]
+  chr = vt100.LITERALS[chr] or chr
+  assert(1 == #chr)
+  push(self.user, chr)
+  info('typo keyinput %q', ev)
+  local w = want[self.wi] or DONE
+  if #self.user < #w then return end
+  local score, u = 0, concat(self.user)
+  info('scoring %q to %q', u, w)
+  -- typed all the characters, calculate score
+  for i=1,#w do
+    if u:sub(i,i) == w:sub(i,i) then score = score + 1 end
+  end
+  ds.clear(self.user)
+  self.wi, self.score = self.wi + 1, self.score + score
 end
 
 getmetatable(M).__call = function(_, ed)
@@ -27,12 +60,7 @@ getmetatable(M).__call = function(_, ed)
     user = {},
   }
   g.actions = {
-    keyinput = function(ed, ev)
-      local chr = ev[1]
-      chr = vt100.LITERALS[chr] or chr
-      assert(1 == #chr)
-      push(g.user, chr)
-    end,
+    keyinput = function(ed, ev) g:keyinput(ed, ev) end,
   }
   ed:focus(g)
 end
